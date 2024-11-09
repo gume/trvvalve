debc4149cdb245385b71620e45bfb48ac101b49c
Gabor Feher <gume@tmit>
2024-11-09 20:41:31 +0100

    bugfix

--- custom_components/trv_valve/valve.py
+++ custom_components/trv_valve/valve.py
@@ -50,17 +50,17 @@ class TRVValve(ValveEntity):
             await self._hub._hass.services.async_call("climate", "set_preset_mode", {"entity_id": self._hub._climate, "preset_mode": "manual"})
         await self._hub._hass.services.async_call("climate", "set_temperature", {"entity_id": self._hub._climate, "temperature": self._hub._open_temp})
         await self._hub._hass.services.async_call("climate", "turn_on", {"entity_id": self._hub._climate})
-        self.__attr_is_closed = False
+        self._attr_is_closed = False
         self._attr_icon = "mdi:valve-open"
         self.schedule_update_ha_state()
 
     async def async_close_valve(self) -> None:
         climate_state = self._hub._hass.states.get(self._hub._climate)
         if climate_state is not None and "manual" in climate_state.attributes.get("preset_modes", []):
-            await self._hub._hass.services.async_call("climate", "set_preset_mode", {"entity_id": self._climate, "preset_mode": "manual"})
+            await self._hub._hass.services.async_call("climate", "set_preset_mode", {"entity_id": self._hub._climate, "preset_mode": "manual"})
         await self._hub._hass.services.async_call("climate", "set_temperature", {"entity_id": self._hub._climate, "temperature": self._hub._close_temp})
         await self._hub._hass.services.async_call("climate", "turn_off", {"entity_id": self._hub._climate})
-        self.__attr_is_closed = True
+        self._attr_is_closed = True
         self._attr_icon = "mdi:valve-closed"
         self.schedule_update_ha_state()
 
